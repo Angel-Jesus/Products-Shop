c665a369c10d2cc0555f6ae006ef6a72d965d606
Angel Jesus <angsus8parriz@gmail.com>
2024-12-30 13:37:29 -0500

    Añadir interfaz de descripcion de producto

--- app/src/main/java/com/angelpr/productsshop/presentation/view/ProductDescriptionScreen.kt
+++ app/src/main/java/com/angelpr/productsshop/presentation/view/ProductDescriptionScreen.kt
@@ -1,5 +1,7 @@
 package com.angelpr.productsshop.presentation.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,10 +11,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -26,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
@@ -50,7 +54,84 @@ fun ProductDescriptionScreen(
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
-            
+            item{
+                Column(
+                    modifier = Modifier
+                        .padding(start = 14.dp, end = 14.dp, top = 8.dp, bottom = 8.dp)
+                ) {
+                    AsyncImage(
+                        modifier = Modifier
+                            .height(300.dp)
+                            .align(Alignment.CenterHorizontally)
+                            .clip(shape = RoundedCornerShape(12.dp)),
+                        model = product.images[0],
+                        contentDescription = null
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = product.title,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.Black
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = product.description,
+                        textAlign = TextAlign.Justify,
+                        fontSize = 14.sp,
+                        color = Color.DarkGray
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "Precio: S/.${product.price}",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.SemiBold
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "Galeria",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.SemiBold
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    LazyRow(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(product.images) { imageUrl ->
+                            AsyncImage(
+                                model = imageUrl,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .width(100.dp)
+                                    .height(100.dp)
+                                    .clip(shape = RoundedCornerShape(8.dp))
+                            )
+                        }
+                    }
+
+
+                }
+            }
         }
 
     }
